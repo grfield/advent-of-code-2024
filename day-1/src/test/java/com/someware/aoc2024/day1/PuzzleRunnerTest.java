51287f6a71f18f0f95614e9100af3a0d27ef27c8
gary <gary.field@pobox.com>
2024-12-01 18:38:44 +0000

    Added beginnings of a unit test and refactored a bit

---
+++ day-1/src/test/java/com/someware/aoc2024/day1/PuzzleRunnerTest.java
@@ -0,0 +1,13 @@
+package com.someware.aoc2024.day1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class PuzzleRunnerTest {
+
+    @Test
+    void calculateSolution() {
+        var runner = new PuzzleRunner("day1/src/test/resources/test-input.txt" );
+
+    }
+}
