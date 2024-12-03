d2d50bbe8649d7694235a45e9b6c47fbe4f88833
Gary Field <gary.field@pobox.com>
2024-12-03 20:00:49 +0000

    Created template for Day 4

---
+++ day-4/src/test/java/com/someware/aoc2024/day4/PuzzleRunnerTest.java
@@ -0,0 +1,14 @@
+package com.someware.aoc2024.day4;
+
+import org.junit.jupiter.api.Test;
+
+class PuzzleRunnerTest {
+
+    @Test
+    void calculatePart1Solution() {
+    }
+
+    @Test
+    void calculatePart2Solution() {
+    }
+}
