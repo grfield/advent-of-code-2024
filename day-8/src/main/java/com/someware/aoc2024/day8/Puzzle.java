b20da4a4302c0173372cdf83ddff002fe014e5cf
Gary Field <gary.field@pobox.com>
2024-12-09 08:36:19 +0000

    Created Day 8 and 9 shell modules

---
+++ day-8/src/main/java/com/someware/aoc2024/day8/Puzzle.java
@@ -0,0 +1,18 @@
+package com.someware.aoc2024.day8;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class Puzzle {
+    static final Logger LOGGER = LoggerFactory.getLogger(Puzzle.class);
+    static final String PUZZLE_INPUT = "day-7/src/main/resources/puzzle-input.txt";
+
+    public static void main(String[] args) throws IOException {
+        LOGGER.info("AoC2024: Day 5");
+        var runner = new PuzzleRunner(PUZZLE_INPUT);
+        LOGGER.info("...part 1 solution: {}", runner.calculatePart1Solution());
+        LOGGER.info("...part 2 solution: {}", runner.calculatePart2Solution());
+    }
+}
---
+++ day-9/src/main/java/com/someware/aoc2024/day9/PuzzleRunner.java
@@ -0,0 +1,59 @@
+package com.someware.aoc2024.day9;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class PuzzleRunner {
+    static final Logger LOGGER = LoggerFactory.getLogger(PuzzleRunner.class);
+    private final String filename;
+
+    public PuzzleRunner(String filename) {
+        this.filename = filename;
+    }
+
+    public long calculatePart1Solution() throws IOException {
+
+
+        return 0;
+    }
+
+    public long calculatePart2Solution() throws IOException {
+
+        return 0;
+    }
+
+
+    private long applyTwoOpsToNumbers(char[] perm, List<Long> list) {
+        long ans = list.getFirst();
+        for (int i = 0; i < perm.length; i++) {
+            ans = switch (perm[i]) {
+                case '+': yield ans + list.get(i+1);
+                case '*': yield ans * list.get(i+1);
+                default: yield 0;
+            };
+        }
+
+        return ans;
+    }
+
+    private long applyThreeOpsToNumbers(char[] perm, List<Long> list) {
+        long ans = list.getFirst();
+        for (int i = 0; i < perm.length; i++) {
+            ans = switch (perm[i]) {
+                case '+': yield ans + list.get(i+1);
+                case '*': yield ans * list.get(i+1);
+                case '|': {
+                    String left = String.valueOf(ans);
+                    String right = String.valueOf(list.get(i+1));
+                    yield Long.parseLong(left + right);
+                }
+                default: yield 0;
+            };
+        }
+
+        return ans;
+    }
+}
