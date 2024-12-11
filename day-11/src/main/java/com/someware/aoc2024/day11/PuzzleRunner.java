0ca7f4bfdff56a5641d881d17016688106d5d58d
Gary Field <gary.field@pobox.com>
2024-12-11 14:44:27 +0000

    Added blank Day 11 and changed names of other modules to be padded with 0 for ordering reasons

---
+++ day-11/src/main/java/com/someware/aoc2024/day11/PuzzleRunner.java
@@ -0,0 +1,27 @@
+package com.someware.aoc2024.day11;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class PuzzleRunner {
+    static final Logger LOGGER = LoggerFactory.getLogger(PuzzleRunner.class);
+    private final String filename;
+
+    public PuzzleRunner(String filename) {
+        this.filename = filename;
+    }
+
+    public long calculatePart1Solution() throws IOException {
+
+
+        return 0;
+    }
+
+    public long calculatePart2Solution() throws IOException {
+
+        return 0;
+    }
+
+}
